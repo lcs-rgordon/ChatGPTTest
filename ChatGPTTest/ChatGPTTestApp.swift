1fe8b35cf90ecd498dc2b2b59f672b5a2d5251e3
Russell Gordon <rgordon@lcs.on.ca>
2024-06-12 09:21:37 -0400

    Initial Commit

---
+++ ChatGPTTest/ChatGPTTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTTestApp.swift
+//  ChatGPTTest
+//
+//  Created by Russell Gordon on 2024-06-12.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
